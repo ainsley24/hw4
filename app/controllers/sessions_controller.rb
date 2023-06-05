9c097434c75a60a9627c3f2eb5e422d91bf23133
Ainsley Campbell <ainsley.campbell@kellogg.northwestern.edu>
2023-06-05 16:26:36 +0000

    first commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,28 @@ class SessionsController < ApplicationController
   end
 
   def create
+    
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome, #{@user["first_name"]}."
+        redirect_to "/places"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    flash["notice"] = "Goodbye."
+    session["current_user"] = nil
+    redirect_to "/login"
   end
 end
+
   
