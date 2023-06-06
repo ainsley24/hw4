35502e907621844d60068f92cb977a9c27aac00a
Ainsley Campbell <ainsley.campbell@kellogg.northwestern.edu>
2023-06-06 00:00:35 +0000

    fixed username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,14 +8,14 @@ class SessionsController < ApplicationController
     if @user
       if BCrypt::Password.new(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
-        flash["notice"] = "Welcome, #{@user["first_name"]}."
+        flash["notice"] = "Welcome, #{@user["username"]}."
         redirect_to "/places"
       else
-        flash["notice"] = "Nope."
+        flash["notice"] = "Invalid login."
         redirect_to "/login"
       end
     else
-      flash["notice"] = "Nope."
+      flash["notice"] = "Invalid login."
       redirect_to "/login"
     end
   end
